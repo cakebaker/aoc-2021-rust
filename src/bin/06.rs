8234fb40d7680b6fbd1ac50982effedd18d62f16
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2022-01-04 10:30:14 +0100

    Add solution for puzzle 1 of day 6

---
+++ src/bin/06.rs
@@ -0,0 +1,50 @@
+use std::env;
+use std::fs;
+
+fn main() -> Result<(), &'static str> {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.is_empty() {
+        return Err("No file specified");
+    }
+
+    let age_list = get_age_list_from_file(&args[0]);
+
+    println!("Result of puzzle 1: {}", part_1(&age_list));
+
+    Ok(())
+}
+
+fn part_1(age_list: &[usize]) -> usize {
+    const MAX_DAYS: usize = 80;
+
+    let mut counters: [usize; 9] = [0; 9];
+
+    for age in age_list {
+        counters[*age] += 1;
+    }
+
+    for _ in 1..=MAX_DAYS {
+        let zeros = counters[0];
+
+        counters.rotate_left(1);
+
+        counters[8] = zeros;
+        counters[6] += zeros;
+    }
+
+    counters.iter().sum()
+}
+
+fn get_age_list_from_file(filename: &str) -> Vec<usize> {
+    let mut file_content =
+        fs::read_to_string(filename).expect("Something went wrong reading the file!");
+
+    // remove newline
+    file_content.pop();
+
+    file_content
+        .split(',')
+        .map(|age| age.parse().unwrap())
+        .collect()
+}
