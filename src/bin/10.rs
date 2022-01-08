bf81e035e44ba536df102336ab5a26b74849b11d
Daniel Hofstetter <daniel.hofstetter@42dh.com>
2022-01-08 16:37:22 +0100

    Add solution for puzzle 2 of day 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -10,7 +10,8 @@ fn main() -> Result<(), &'static str> {
 
     let navigation_subsystem = get_navigation_subsystem_from_file(&args[0]);
 
-    println!("Result of puzzle 1: {:?}", part_1(&navigation_subsystem));
+    println!("Result of puzzle 1: {}", part_1(&navigation_subsystem));
+    println!("Result of puzzle 2: {}", part_2(&navigation_subsystem));
 
     Ok(())
 }
@@ -38,8 +39,49 @@ fn part_1(navigation_subsystem: &[Vec<char>]) -> usize {
     error_scores.iter().sum()
 }
 
+fn part_2(navigation_subsystem: &[Vec<char>]) -> usize {
+    let mut scores = Vec::new();
+
+    for line in navigation_subsystem {
+        let mut stack = Vec::new();
+        let mut is_corrupt = false;
+
+        for c in line {
+            if c.is_open() {
+                stack.push(c);
+            } else {
+                let elem = stack.pop().unwrap();
+
+                if c.is_incorrect_close_for(*elem) {
+                    is_corrupt = true;
+                    break;
+                }
+            }
+        }
+
+        if !is_corrupt {
+            let mut score = 0;
+            stack.reverse();
+
+            for item in stack {
+                score *= 5;
+                score += item.get_score();
+            }
+
+            scores.push(score);
+        }
+    }
+
+    scores.sort();
+
+    let middle = scores.len() / 2;
+
+    scores[middle]
+}
+
 trait ChunkChar {
     fn get_error_score(&self) -> usize;
+    fn get_score(&self) -> usize;
     fn is_incorrect_close_for(&self, open: char) -> bool;
     fn is_open(&self) -> bool;
 }
@@ -51,6 +93,16 @@ impl ChunkChar for char {
             ']' => 57,
             '}' => 1197,
             '>' => 25137,
+            _ => 0,
+        }
+    }
+
+    fn get_score(&self) -> usize {
+        match self {
+            '(' => 1,
+            '[' => 2,
+            '{' => 3,
+            '<' => 4,
             _ => 0,
         }
     }
